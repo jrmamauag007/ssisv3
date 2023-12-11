8d968d3b45f754e06ecb975be6ad86bac8ed7188
John Robert Mamauag <johnrobert.mamauag@g.msuiit.edu.ph>
2023-12-12 03:08:32 +0800

    Added js search function

--- app/static/js/master.js
+++ app/static/js/master.js
@@ -27,3 +27,36 @@ setTimeout(function() {
       });
     });
   });
+
+    // Search Script
+$(document).ready(function() {
+  // Listen for changes in the search input field
+  $("#search-input").on("input", function() {
+      var searchText = $(this).val().toLowerCase();
+
+      // Loop through table rows and hide those that don't match the search text
+      $(".highlightable-row").each(function() {
+          // Get all td elements
+          var tdsToSearch = $(this).find('td');
+
+          // Concatenate the text content of each td
+          var rowText = tdsToSearch.map(function() {
+              return $(this).text().toLowerCase();
+          }).get().join(' ');
+
+          // Check if the rowText contains the search text
+          if (rowText.indexOf(searchText) === -1) {
+              $(this).hide();
+          } else {
+              $(this).show();
+          }
+      });
+});
+  $(".highlightable-row").each(function () {
+    var status = $(this).find('td:eq(2)').text().trim().toLowerCase();
+    if (status === "resolved") {
+        $(this).addClass("table-secondary");
+        $(this).find('td').css('font-weight', '300');
+    }
+});
+});
