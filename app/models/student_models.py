d10a1043bff8d7c32a2e99fb5da19b22c651b8ec
John Robert Mamauag <johnrobert.mamauag@g.msuiit.edu.ph>
2023-12-23 11:19:19 +0800

    Refactor college column display in students table

--- app/models/student_models.py
+++ app/models/student_models.py
@@ -7,7 +7,7 @@ class StudentModel:
         connection = mysql.connection
         cursor = connection.cursor(dictionary=True)
 
-        cursor.execute("SELECT *, collegecode FROM Students JOIN Courses on Students.coursecode = Courses.coursecode")
+        cursor.execute("SELECT * FROM Students JOIN Courses on Students.coursecode = Courses.coursecode JOIN Colleges on Courses.collegecode = Colleges.collegecode ORDER BY id ASC")
         students = cursor.fetchall()
 
         cursor.close()
