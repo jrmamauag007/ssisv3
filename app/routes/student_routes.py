8064ee81dfabdc1e5a8c257bdb725d664371a301
John Robert Mamauag <johnrobert.mamauag@g.msuiit.edu.ph>
2023-12-23 11:19:50 +0800

    Enforce 1mb limit on img files

--- app/routes/student_routes.py
+++ app/routes/student_routes.py
@@ -42,12 +42,17 @@ def add_student():
             return redirect(url_for('student.get_all_students'))
         # Check if a file was provided
         if image:
-            # Convert image data size from bytes to megabytes
-            image_size_mb = image.content_length / (1024 * 1024)
+            # Get the size of the file in bytes
+            image_size_bytes = len(image.read())
+            
+            # Reset the file cursor back to the beginning for further processing
+            image.seek(0)
+
+            image_size_mb = image_size_bytes / (1024 * 1024)
 
             # Check if the image size exceeds the limit
-            if image_size_mb > 5:
-                flash(f'Image size exceeds the maximum limit of {5} MB', 'error')
+            if image_size_mb > 1:
+                flash(f'Image size exceeds the maximum limit of {1} MB', 'error')
                 return redirect(url_for('student.get_all_students'))
             
             filename = secure_filename(request.files['addFile'].filename)
@@ -107,21 +112,26 @@ def update_student():
         image = request.files.get('editFile')
         student_id = id
         image_url = None
+    
 
-
-        print(image.content_length)
         # Check if a file was provided
         if image:
             # Convert image data size from bytes to megabytes
-            image_size_mb = image.content_length / (1024 * 1024)
+
+            # Get the size of the file in bytes
+            image_size_bytes = len(image.read())
+            
+            # Reset the file cursor back to the beginning for further processing
+            image.seek(0)
+
+            image_size_mb = image_size_bytes / (1024 * 1024)
             print(image_size_mb)
 
             # Check if the image size exceeds the limit
-            if image_size_mb > 5:
-                flash(f'Image size exceeds the maximum limit of {5} MB', 'error')
-                return redirect(url_for('student.get_students', student_id=student_id))
-            print("1")
-            print("2")
+            if image_size_mb > 1:
+                flash(f'Image size exceeds the maximum limit of {1} MB', 'error')
+                return redirect(url_for('student.get_all_students'))
+
 
             # Check if the file extension is allowed
             filename = secure_filename(request.files['editFile'].filename)
@@ -137,9 +147,9 @@ def update_student():
                 cloudinary.uploader.destroy(public_id)
                 print(public_id)
 
-            print("3")
+            
             upload_result = cloudinary.uploader.upload(image)
-            print("4")
+        
             image_url = upload_result['url']
         
             student_model_instance = StudentModel()
@@ -159,7 +169,7 @@ def update_student():
         flash('Student edit successfully', 'success')
     except Exception as e:
         print(e)
-        flash('Failed to edit Student', 'error')
+        flash('Failed to edit Student. Error: {}'.format(str(e)), 'error')
 
     return redirect(url_for('student.get_all_students'))
 
@@ -192,12 +202,17 @@ def upload_image():
         
         # Check if a file was provided
         if image:
-            # Convert image data size from bytes to megabytes
-            image_size_mb = image.content_length / (1024 * 1024)
+            # Get the size of the file in bytes
+            image_size_bytes = len(image.read())
+            
+            # Reset the file cursor back to the beginning for further processing
+            image.seek(0)
+
+            image_size_mb = image_size_bytes / (1024 * 1024)
 
             # Check if the image size exceeds the limit
-            if image_size_mb > 5:
-                flash(f'Image size exceeds the maximum limit of {5} MB', 'error')
+            if image_size_mb > 1:
+                flash(f'Image size exceeds the maximum limit of {1} MB', 'error')
                 return redirect(url_for('student.get_student', student_id=student_id))
             
             # Check if the file extension is allowed
@@ -227,7 +242,7 @@ def upload_image():
             return redirect(url_for('student.get_student', student_id=student_id))
 
     except Exception as e:
-        flash('Failed to upload image', 'error')
+        flash('Failed to upload image. Error: {}'.format(str(e)), 'error')
         print(str(e))  # Log the exception for debugging
 
     return redirect(url_for('student.get_all_students'))
